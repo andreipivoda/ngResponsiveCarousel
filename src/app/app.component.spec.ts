f5492292c7f3afb56d094d6583fa4e32bb16d431
Andrei Pivoda <andrei.pivoda.external@autoliv.com>
2020-01-31 12:49:54 +0200

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap' ;
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
-      ],
+      ],imports:[NgbModule],
     }).compileComponents();
   }));
 
@@ -16,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngResponsiveCarousel'`, () => {
+  it(`should have as title 'Angular Responsive Carousel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngResponsiveCarousel');
+    expect(app.title).toEqual('Angular Responsive Carousel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngResponsiveCarousel app is running!');
+    expect(compiled.querySelector('p').textContent).toContain('Better check yourself');
   });
 });
