54f8f9c464bf772d3884c7cb9172e9304dc92029
Andrei Pivoda <andrei.pivoda.external@autoliv.com>
2020-01-31 14:59:27 +0200

    +carousel comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgbSlideEvent, NgbSlideEventSource, NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Responsive Carousel';
+  @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
+
+  ngOnInit() { }
+
+  // Move to specific slide
+  navigateToSlide(item) {
+    this.ngCarousel.select(item);
+    console.log(item)
+  }
+
+  // Move to previous slide
+  getToPrev() {
+    this.ngCarousel.prev();
+  }
+
+  // Move to next slide
+  goToNext() {
+    this.ngCarousel.next();
+  }
+
+  // Pause slide
+  stopCarousel() {
+    this.ngCarousel.pause();
+  }
+
+  // Restart carousel
+  restartCarousel() {
+    this.ngCarousel.cycle();
+  }
 }
