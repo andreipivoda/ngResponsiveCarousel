c670646ff098f2a5745a34731aaed574416f3a04
Andrei Pivoda <andrei.pivoda.external@autoliv.com>
2020-01-31 16:27:48 +0200

    ngclass fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,11 @@ import { HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Responsive Carousel';
+  pop: boolean;
   key: any;
   @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
 
-  currentStep = 'Auto';
+  currentStep = 'Auto Playback';
   ngOnInit() { }
 
   @HostListener('document:keydown', ['$event'])
@@ -22,17 +23,33 @@ export class AppComponent {
     this.key = event.key;
     // console.log(event.code);
     if (event.code === 'F5' || event.code === 'Escape') {
+      this.pop = true;
       console.log('PLAY');
       this.restartCarousel();
+      setTimeout (() => {
+        console.log('fade');
+        this.pop = false;
+     }, 1000);
     } else if (event.code === 'Period') {
+      this.pop = true;
       console.log('STOP');
       this.stopCarousel();
     } else if (event.code === 'PageUp') {
+      this.pop = true;
       console.log('PREVIOUS');
       this.getToPrev();
+      setTimeout(() => {
+        console.log('fade');
+        this.pop = false;
+      }, 1000);
     } else if (event.code === 'PageDown') {
+      this.pop = true;
       console.log('NEXT');
       this.goToNext();
+      setTimeout(() => {
+        console.log('fade');
+        this.pop = false;
+      }, 1000);
     }
   }
 
@@ -44,11 +61,15 @@ export class AppComponent {
 
   // Move to previous slide
   getToPrev() {
+    this.currentStep = 'Showing Previous';
+    this.ngCarousel.pause();
     this.ngCarousel.prev();
   }
 
   // Move to next slide
   goToNext() {
+    this.currentStep = 'Showing Next';
+    this.ngCarousel.pause();
     this.ngCarousel.next();
   }
 
@@ -60,7 +81,8 @@ export class AppComponent {
 
   // Restart carousel
   restartCarousel() {
-    this.currentStep = 'Auto';
+    this.currentStep = 'Auto Playback';
     this.ngCarousel.cycle();
   }
 }
+
