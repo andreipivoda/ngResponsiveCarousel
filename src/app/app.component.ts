406d3867e9e6f8700d7d0c3443b04e4d46440a19
Andrei Pivoda <andrei.pivoda.external@autoliv.com>
2020-01-31 15:47:58 +0200

    set kcode => action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,38 @@ import { HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular Responsive Carousel';
+  key: any;
   @ViewChild('ngcarousel', { static: true }) ngCarousel: NgbCarousel;
 
+  currentStep = 'Auto';
   ngOnInit() { }
 
-  // Move to specific slide
-  navigateToSlide(item) {
-    this.ngCarousel.select(item);
-    console.log(item)
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    event.returnValue = false;
+    event.preventDefault();
+    this.key = event.key;
+    // console.log(event.code);
+    if (event.code === 'F5' || event.code === 'Escape') {
+      console.log('PLAY');
+      this.restartCarousel();
+    } else if (event.code === 'Period') {
+      console.log('STOP');
+      this.stopCarousel();
+    } else if (event.code === 'PageUp') {
+      console.log('PREVIOUS');
+      this.getToPrev();
+    } else if (event.code === 'PageDown') {
+      console.log('NEXT');
+      this.goToNext();
+    }
   }
+
+  // Move to specific slide
+  // navigateToSlide(item) {
+  //   this.ngCarousel.select(item);
+  //   console.log(item)
+  // }
 
   // Move to previous slide
   getToPrev() {
@@ -31,11 +54,13 @@ export class AppComponent {
 
   // Pause slide
   stopCarousel() {
+    this.currentStep = 'Paused';
     this.ngCarousel.pause();
   }
 
   // Restart carousel
   restartCarousel() {
+    this.currentStep = 'Auto';
     this.ngCarousel.cycle();
   }
 }
